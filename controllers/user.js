7f84ecaa26c4a6cfd2541b15c83d735a4cbbe81e
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-12-11 17:20:44 +0530

    middleware to update purchase list

--- controllers/user.js
+++ controllers/user.js
@@ -57,6 +57,7 @@ exports.userPurchaseList = (req, res) => {
     Order.find({
         user: req.profile._id
     })
+        // populate helps to reference document in other collection
         .populate("user", "_id name") //this is populate synatx no comma there
         .exec((err, order) => {
             if (err) {
@@ -64,6 +65,35 @@ exports.userPurchaseList = (req, res) => {
                     error: "No order in this account"
                 })
             }
-            return res.json;
+            return res.json(order);
         })
+}
+
+exports.pushOrderInPurchaseList = (req, res, next) => {
+    let purchases = []
+    req.body.order.products.forEach(product => {
+        purchases.push({
+            _id: product._id,
+            name: product.name,
+            description: product.description,
+            category: product.category,
+            quantity: product.quantity,
+            amount: req.body.order.amount,
+            transaction_id: req.body.order.transaction_id
+        })
+    })
+    //  store this in db
+    User.findOneAndUpdate(
+        { _id: req.profile._id },
+        { $push: { purchases: purchases } },
+        { new: true },
+        (err, purchases) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Unable to save purchase list";
+                })
+            }
+            next();
+        }
+    )
 }
