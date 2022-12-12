5d7ff0d250ad3489b67ce1e418a90ffb73a8c5be
Dwarikanath1502 <ddwarikanath15@gmail.com>
2022-12-12 15:06:14 +0530

    get category and getAllCategory

--- controllers/user.js
+++ controllers/user.js
@@ -96,4 +96,4 @@ exports.pushOrderInPurchaseList = (req, res, next) => {
             next();
         }
     )
-}
+} 
--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
-const {getCategoryById} = require('../controllers/category')
-const {isAuthenticated, isAdmin, isSignedIn} = require('../controllers/auth')
-const {getUserById} = require('../controllers/user')
+const { getCategoryById, getCategory,createCategory, getAllCategory } = require('../controllers/category')
+const { isAuthenticated, isAdmin, isSignedIn } = require('../controllers/auth')
+const { getUserById } = require('../controllers/user')
 
+// PARAMS
 router.param("userId", getUserById)
 router.param("categoryId", getCategoryById)
 
-
+// ACTUAL ROUTES GOES HERE
+router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
+router.get("category/:categoryId", getCategory)
+router.get("category", getAllCategory)
 
 module.exports = router;
